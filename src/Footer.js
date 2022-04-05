a3edfabd82051b7800e7ea440610df769e24c595
Seremba <pserembae@gmai.com>
2022-04-05 09:08:44 +0300

    learning react from 4:30 to 7:00

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Footer() {
+    const today = new Date();
+  return (
+    <footer>
+        copyright &copy; , {today.getFullYear()}
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/Header..js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>Grocery List</header>
+  )
+}
+
+export default Header
